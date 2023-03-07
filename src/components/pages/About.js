7802ee2784b60a27f2097aa58616e81cb6d611bc
Lawzstopwar <lawzstopwar@gmail.com>
2023-03-07 23:48:19 +0000

    Home page and project information updated

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,11 +5,9 @@ function About() {
     <div>
       <h1>About Page</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      To develop the initial scaffolding for my first portfolio site
+       as well as to create and update other materials so that I can showcase my skills to potential employers 
+       as creating a portfolio using React has its potential advantages in compare to other technologies.
       </p>
     </div>
   );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,21 +1,26 @@
 import React from "react";
+import Placeholder from 'react-bootstrap/Placeholder';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-function Home() {
+
+
+
+function WithHeaderStyledExample() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
-        varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
-        Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-        imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
-        ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
-        elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-        consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
-        malesuada fames ac ante ipsum primis in faucibus.
-      </p>
-    </div>
+    <Card>
+      <Card.Header as="h3">Home Page</Card.Header>
+      <Card.Body>
+      <Placeholder xs={12} bg="success" />
+        <Card.Title>Project Title :  React Portolio</Card.Title>
+        <Placeholder xs={12} bg="primary" />
+        <Card.Text> Name :  Lawrence Stopwar</Card.Text>
+        <Placeholder xs={12} bg="danger" />
+        <Button variant="Github Page">https://github.com/Lawzstopwar/react-routing-portfolio/settings</Button>
+        <Placeholder xs={12} bg="success" />
+      </Card.Body>
+    </Card>
   );
 }
 
-export default Home;
+export default WithHeaderStyledExample;
